12a4ed0331fe1a96c9a0abce9a3409e6effd1774
pradip bhatt <bhattsammar04@gmail.com>
2024-10-11 19:34:02 +0545

    Update server.js
    Online mongodbURI added

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ dotenv.config(); // Load environment variables
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const mongoUrl = "mongodb://localhost:27017/pradipblogs";
+const mongoUrl = "mongodb+srv://pradipbhatt:pradip@cluster0.zi62tma.mongodb.net/blogs?retryWrites=true&w=majority&appName=Cluster0";
 
 // Middleware
 app.use(cors());
